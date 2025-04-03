993853066ee1d11639c8ac72e3731d545522d2d2
sameer2210 <sameerkhan27560@gmail.com>
2025-04-04 01:49:21 +0530

    Razorpay_A

---
+++ mongoDB/Day12 (Razorpay_A)/payment.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+//   const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+  orderId: {
+    type: String,
+    required: true,
+  },
+  paymentId: {
+    type: String,
+  },
+  signature: {
+    type: String,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: 'pending',
+  },
+}, { timestamps: true });
+
+const Payment = mongoose.model('Payment', paymentSchema);
+
+module.exports = Payment;
