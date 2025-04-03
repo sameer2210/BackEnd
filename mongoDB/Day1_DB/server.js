5bd8e82685b674b74cba34265cf53d240a72ca8a
sameer2210 <sameerkhan27560@gmail.com>
2025-04-04 03:06:33 +0530

    update_DB

---
+++ mongoDB/Day1_DB/server.js
@@ -0,0 +1,16 @@
+// npm init -y
+// npm i express 
+
+// server.js     (server start krana hai // DB connect krna hai )
+// .src/app.js   (server config krna hai)
+// .src/db/db.js (db se kese connect kroge)
+
+
+const app = require("./src/app");
+const connect = require("./src/db/db");
+
+connect()
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000...");
+});
