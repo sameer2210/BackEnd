669fa97094555c312b60d376a7f6403757e5da6f
sameer2210 <sameerkhan27560@gmail.com>
2025-04-15 03:43:44 +0530

    google_auth

--- mongoDB/Day15_GoogleAuth/app.js
+++ mongoDB/Day15_GoogleAuth/app.js
@@ -16,6 +16,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/callback"
+      // callbackURL: process.env.GOOGLE_CALLBACK_URL
     },
     (accessToken, refreshToken, profile, done) => {
       // Here, you would typically find or create a user in your database
